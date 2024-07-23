708bc6d716ca252eafce8c2b750e35285ab22094
Pedro Freitas <pedro.sidra@senairs.org.br>
2024-07-23 11:06:06 -0300

    feat: add clearml runner

--- run.py
+++ run.py
@@ -1,21 +1,41 @@
 import clearml
+import requests
 import clearml.datasets
 import subprocess
 from pathlib import Path
 import os
 
-task : clearml.Task = clearml.Task.init(
-    project_name="mestre_pedro",
-    task_name="msc-train"
+task: clearml.Task = clearml.Task.init(
+    project_name="mestre_pedro", task_name="msc-train"
 )
-task.set_base_docker(docker_image="pedrosidra0/pointcept:v0")
-task.execute_remotely()
+task.set_base_docker(
+    docker_image="pedrosidra0/pointcept:v0",
+    docker_arguments="--shm-size=64000mb -e MKL_SERVICE_FORCE_INTEL=1",
+)
+task.execute_remotely(queue_name="default")
 
-data_path = clearml.datasets.Dataset.get(dataset_id="a38dbe849a49415f99196916d4f947c5").get_local_copy()
+data_path = clearml.datasets.Dataset.get(
+    dataset_id="a38dbe849a49415f99196916d4f947c5"
+).get_local_copy()
 
-dataset_path = "./data/scannet"
-if not Path(dataset_path).exists():
+dataset_path = Path("./data/scannet")
+if not dataset_path.exists():
     print(f"Symlinking {data_path} to {dataset_path}")
+    dataset_path.parent.mkdir(exist_ok=True, parents=True)
     os.symlink(src=data_path, dst=dataset_path)
 
-subprocess.run("sh scripts/train.sh -g 2 -d scannet -c pretrain-msc-v1m1-0-spunet-base -n pretrain-msc-v1m1-0-spunet-base")
+pretrain_link = "http://10.167.1.54:8081/mestre_pedro/msc-train.3ae6e83760764352bf3f2a62a51f138b/artifacts/model/model_last.pth"
+pretrain_path = "pretrain_model.pth"
+
+r = requests.get(pretrain_link)
+open(pretrain_path , 'wb').write(r.content)
+
+# subprocess.run(
+#     "sh scripts/train.sh -g 2 -d scannet -c pretrain-msc-v1m1-0-spunet-base -n pretrain-msc-v1m1-0-spunet-base".split()
+# )
+subprocess.run(
+    f"sh scripts/train.sh -g 2 -d scannet -w {pretrain_path} -c semseg-spunet-v1m1-4-ft -n semseg-msc-v1m1-0f-spunet-base".split()
+)
+
+model_path = next(Path("exp").glob("**/model/*.pth"))
+task.upload_artifact(name="model", artifact_object=str(model_path))
