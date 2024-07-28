2007935cfc331860fa64302087f8a9e805aaf7b0
Pedro Sidra <pedro.sidra@senairs.org.br>
2024-07-28 21:44:13 +0000

    add forked pdb

--- pointcept/datasets/utils.py
+++ pointcept/datasets/utils.py
@@ -11,6 +11,21 @@ import numpy as np
 import torch
 from torch.utils.data.dataloader import default_collate
 
+import sys
+import pdb
+
+class ForkedPdb(pdb.Pdb):
+    """A Pdb subclass that may be used
+    from a forked multiprocessing child
+
+    """
+    def interaction(self, *args, **kwargs):
+        _stdin = sys.stdin
+        try:
+            sys.stdin = open('/dev/stdin')
+            pdb.Pdb.interaction(self, *args, **kwargs)
+        finally:
+            sys.stdin = _stdin
 
 def collate_fn(batch):
     """
