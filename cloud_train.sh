7035ea7fc6fe8cef697804ddfe69aa269e9c9736
Pedro Freitas <pedro.sidra@senairs.org.br>
2025-01-20 16:40:02 +0000

    update cloud scripts

--- cloud_train.sh
+++ cloud_train.sh
@@ -1,9 +1,9 @@
 set -e
 
-pip install huggingface-cli wandb
+pip install -U "huggingface_hub[cli]" wandb
 mkdir -p /workspace/Pointcept/data/
 cd /workspace/Pointcept/data/
-wandb artifact get --type dataset --root . pedrosidra/dataset/scannet:v0&
+wandb artifact get --type dataset --root ./scannet pedrosidra/dataset/scannet:v0&
 huggingface-cli download --repo-type dataset  --local-dir . --  Pointcept/arkitscenes-compressed
 for i in 1 2 3 4 5 6 7 8; do tar -xzvf arkitscenes_$i.tar.gz; done
 mv Training/ scannet/arkit/
