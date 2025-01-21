b6132513e350f968e897b9abbc3dd40a6279fd62
Pedro Freitas <pedro.sidra@senairs.org.br>
2025-01-21 18:40:26 +0000

    fix: scripts

--- cloud_train.sh
+++ cloud_train.sh
@@ -1,13 +1,6 @@
 set -e
 
-pip install -U "huggingface_hub[cli]" wandb
-mkdir -p /workspace/Pointcept/data/
-cd /workspace/Pointcept/data/
-wandb artifact get --type dataset --root ./scannet pedrosidra/dataset/scannet:v0&
-huggingface-cli download --repo-type dataset  --local-dir . --  Pointcept/arkitscenes-compressed
-for i in 1 2 3 4 5 6 7 8; do tar -xzvf arkitscenes_$i.tar.gz; done
-mv Training/ scannet/arkit/
-
 cd /workspace/Pointcept
+bash setup_datasets.sh
 bash scripts/train.sh -g 2 -d scannet -c pipeline-sculpting-spunet-arkit -n icip/sculpt_multi
 
