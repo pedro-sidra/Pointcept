422a6e1373e64bf04bbd338be9f3c3735052dd5d
Pedro Freitas <pedro.sidra@senairs.org.br>
2025-01-21 18:40:26 +0000

    fix: scripts

--- setup_datasets.sh
+++ setup_datasets.sh
@@ -5,5 +5,6 @@ mkdir -p /workspace/Pointcept/data/
 cd /workspace/Pointcept/data/
 wandb artifact get --type dataset --root ./scannet pedrosidra/dataset/scannet:v0&
 huggingface-cli download --repo-type dataset  --local-dir . --  Pointcept/arkitscenes-compressed
-for i in 1 2 3 4 5 6 7 8; do tar -xzvf arkitscenes_$i.tar.gz; done
+for i in 1 2 3 4 5 6 7 8; do tar -xzvf arkitscenes_$i.tar.gz --no-same-owner & done
+wait
 mv Training/ scannet/arkit/
