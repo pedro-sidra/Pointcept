0f89d9c95771f694821d87b49b54db8bda37b029
Pedro Sidra de Freitas <pedro.sidra@senairs.org.br>
2025-02-01 19:17:25 +0000

    train params

--- cloud_train.sh
+++ cloud_train.sh
@@ -2,5 +2,5 @@ set -e
 
 cd /workspace/Pointcept
 bash setup_datasets.sh
-bash scripts/train.sh -g 2 -d scannet -c pipeline-sculpting-spunet-arkit -n icip/sculpt_multi
+bash scripts/train.sh -g 2 -d scannet -c pipeline-sculpting-spunet -n icip/sculpt_arkit
 
