d4e877def8e27fe3811bb2416c0f282b7b11f40b
Pedro Freitas <pedro.sidra@senairs.org.br>
2025-05-30 15:48:38 -0300

    refactor: move pointcept stuff to submodule

--- pointcept/models/masked_scene_contrast/__init__.py
+++
@@ -1,3 +0,0 @@
-from .masked_scene_contrast_v1m1_base import MaskedSceneContrast
-from .masked_scene_contrast_v1m2_csc import MaskedSceneContrast
-from .masked_scene_contrast_sculpting import MaskedSceneContrast
